2020b20bf0beceaaf131f40828021c00061ba5e3
RN Developer <allen.gzqyl@gmail.com>
2024-03-05 17:05:08 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,100 +3,6 @@
 
 import PackageDescription
 
-#if os(Linux)
-
-let platformTargets: [Target] = [
-    .target(name: "SwiftPlot"),
-    
-    // AGG renderer and dependencies.
-    .systemLibrary(
-        name: "CFreeType",
-        pkgConfig: "freetype2",
-        providers: [.brew(["freetype2"]), .apt(["libfreetype6-dev"])]),
-    .target(
-        name: "AGG",
-        dependencies: ["CFreeType"]),
-    .target(name: "lodepng"),
-    .target(
-        name: "CPPAGGRenderer",
-        dependencies: ["AGG","lodepng"],
-        path: "Sources/AGGRenderer/CPPAGGRenderer"),
-    .target(
-        name: "CAGGRenderer",
-        dependencies: ["CPPAGGRenderer"],
-        path: "Sources/AGGRenderer/CAGGRenderer"),
-    .target(
-        name: "AGGRenderer",
-        dependencies: ["CAGGRenderer","SwiftPlot"],
-        path: "Sources/AGGRenderer/AGGRenderer"),
-
-    // Other renderers.
-    .target(
-        name: "SVGRenderer",
-        dependencies: ["SwiftPlot"]),
-    
-    .testTarget(
-      name: "SwiftPlotTests",
-      dependencies: ["AGGRenderer", "SVGRenderer", "SwiftPlot"])
-]
-let platformProducts: [Product] =  [
-  .library(name: "SwiftPlot", targets: ["SwiftPlot"]),
-  .library(name: "SVGRenderer", targets: ["SVGRenderer"]),
-  .library(name: "AGGRenderer", targets: ["AGGRenderer"]),
-]
-
-// Building for iOS/tvOS/watchOS:
-// Change the condition below to "!os(macOS)".
-#elseif os(macOS)
-
-let platformTargets: [Target] = [
-    .target(name: "SwiftPlot"),
-    
-    // AGG renderer and dependencies.
-    .systemLibrary(
-        name: "CFreeType",
-        pkgConfig: "freetype2",
-        providers: [.brew(["freetype2"]), .apt(["libfreetype6-dev"])]),
-    .target(
-        name: "AGG",
-        dependencies: ["CFreeType"]),
-    .target(name: "lodepng"),
-    .target(
-        name: "CPPAGGRenderer",
-        dependencies: ["AGG","lodepng"],
-        path: "Sources/AGGRenderer/CPPAGGRenderer"),
-    .target(
-        name: "CAGGRenderer",
-        dependencies: ["CPPAGGRenderer"],
-        path: "Sources/AGGRenderer/CAGGRenderer"),
-    .target(
-        name: "AGGRenderer",
-        dependencies: ["CAGGRenderer","SwiftPlot"],
-        path: "Sources/AGGRenderer/AGGRenderer"),
-
-    // Other renderers.
-    .target(
-        name: "SVGRenderer",
-        dependencies: ["SwiftPlot"]),
-    .target(
-        name: "QuartzRenderer",
-        dependencies: ["SwiftPlot"]),
-    
-    .testTarget(
-      name: "SwiftPlotTests",
-      dependencies: [
-        "AGGRenderer",
-        "SVGRenderer", "QuartzRenderer", "SwiftPlot"])
-]
-let platformProducts: [Product] =  [
-  .library(name: "SwiftPlot", targets: ["SwiftPlot"]),
-  .library(name: "SVGRenderer", targets: ["SVGRenderer"]),
-  .library(name: "AGGRenderer", targets: ["AGGRenderer"]),
-  .library(name: "QuartzRenderer", targets: ["QuartzRenderer"])
-]
-
-#elseif os(iOS) || os(tvOS) || os(watchOS) || os(macOS)
-
 let platformTargets: [Target] = [
     .target(name: "SwiftPlot"),
     
@@ -111,13 +17,12 @@ let platformTargets: [Target] = [
       name: "SwiftPlotTests",
       dependencies: ["SVGRenderer", "QuartzRenderer", "SwiftPlot"])
 ]
+
 let platformProducts: [Product] =  [
   .library(name: "SwiftPlot", targets: ["SwiftPlot"]),
   .library(name: "SVGRenderer", targets: ["SVGRenderer"]),
   .library(name: "QuartzRenderer", targets: ["QuartzRenderer"])
 ]
-
-#endif
 
 let package = Package(
     name: "SwiftPlot",
